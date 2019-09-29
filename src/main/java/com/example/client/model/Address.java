9347c0a486a60669f1fb9146fb7fe02905e08540
rajesh <rajeshketha1@yahoo.com>
2019-09-29 15:44:28 +0530

    implemented active profile for yml

--- src/main/java/com/example/client/model/Address.java
+++ src/main/java/com/example/client/model/Address.java
@@ -2,10 +2,8 @@ package com.example.client.model;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
 @ConfigurationProperties(prefix = "address")
 public class Address {
 
